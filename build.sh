cfcb4adb2228fb5ac92b0d40bb7c6f89bccf219c
formix <jeanphilippe.gravel@gmail.com>
2020-06-24 15:10:58 -0400

    updated sio2.z80

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+set -e      # Exit on error
+
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo -e "Building bios.z80:\n"
-zasm -uyw bios.z80
 mkdir -p out
-mv bios.rom out
+zasm -uyw bios.z80 -o out/bios.rom
 echo bios.rom... done!
-mv bios.lst out
-echo bios.lst... done!
 cp retroz-bios-inc.header out/retroz-bios-inc.z80
 echo -e "\n; +++ global symbols +++\n" >> out/retroz-bios-inc.z80
 ./export.py < out/bios.lst | sort >> out/retroz-bios-inc.z80
 echo -e "bios-inc.z80... done!"
 echo All done! Output files are in ./out enjoy!
-echo To call bios subroutines, include out/bios-inc.z80 in your assemler files.
+echo To call bios subroutines, include out/bios-inc.z80 in your source files.
